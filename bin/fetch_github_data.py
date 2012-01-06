519274b632a5d2012f26500b278ae74f8411725d
Petter Eriksson <periksson@splunk.com>
2012-01-05 22:07:24 -0800

    Made the fetch_github_data.py into handling multiple github repository addresses.
    Iterates over the github_user_repos that's returned
    from splunkgit_settings, instead of getting a single
    one.

--- bin/fetch_github_data.py
+++ bin/fetch_github_data.py
@@ -26,17 +26,14 @@ from SplunkAPI import SplunkAPI
 from GithubAPI import GithubAPI
 import splunkgit_settings
 
-GITHUB_USER = splunkgit_settings.github_user_login_name()
-GITHUB_REPO = splunkgit_settings.github_repo_name()
-
-    
-if __name__ == '__main__':
-    github_api = GithubAPI(GITHUB_USER, GITHUB_REPO)
+def fetch_single_github_repo_data(github_user, github_repo):
+    github_api = GithubAPI(github_user, github_repo)
     time_stamp = strftime("%Y-%m-%d %H:%M:%S %z", localtime())    
 
     repo = github_api.repo()
-    print '[{0}] github_watcher_count={1}'.format(time_stamp, repo['watchers'])
-    print '[{0}] github_forks_count={1}'.format(time_stamp, repo['forks'])
+    if repo is not None:
+        print '[{0}] github_watcher_count={1}'.format(time_stamp, repo['watchers'])
+        print '[{0}] github_forks_count={1}'.format(time_stamp, repo['forks'])
 
     splunk_api = SplunkAPI(splunkgit_settings.splunk_user_name(), splunkgit_settings.splunk_password())
     since = splunk_api.time_of_last_updated_issue()
@@ -45,3 +42,9 @@ if __name__ == '__main__':
     all_issues =  github_api.issues_since(since)
     for issue in all_issues :
         print u'[{0}] github_issue_number={1} github_issue_state="{2}" github_issue_comment_count={3} github_issue_reporter="{4}" github_issue_title="{5}" github_issue_close_time="{6}" github_issue_update_time="{7}" github_issue_creation_time="{8}"'.format(time_stamp, issue['number'], issue['state'], issue['comments'], issue['user']['login'], issue['title'], issue['closed_at'], issue['updated_at'], issue['created_at'])
+
+if __name__ == '__main__':
+    for github_user_repo in splunkgit_settings.github_user_repos():
+        github_user = github_user_repo.get_user()
+        github_repo = github_user_repo.get_repo()
+        fetch_single_github_repo_data(github_user, github_repo)
