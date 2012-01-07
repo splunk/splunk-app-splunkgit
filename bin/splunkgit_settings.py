ec109480dca714e4b0badd052f49648da8e560a0
Petter Eriksson <periksson@splunk.com>
2012-01-06 16:37:41 -0800

    Renamed GithubUserRepo to GithubRepository. In addition it now contains the repo_address aswell.
    This change is useful for fetch_github_data.py when
    fetching github issues, because it needs the
    repo_address.

--- bin/splunkgit_settings.py
+++ bin/splunkgit_settings.py
@@ -23,17 +23,45 @@ SPLUNKGIT_GIT_SETTINGS = splunk.clilib.cli_common.getConfStanza('splunkgit','git
 SPLUNKGIT_GITHUB_SETTINGS = splunk.clilib.cli_common.getConfStanza('splunkgit','github')
 SPLUNK_SETTINGS = splunk.clilib.cli_common.getConfStanza('splunkgit','splunk')
 
-class GithubUserRepo(object):
+class GithubRepository(object):
 
-    def __init__(self, user, repo):
+    def __init__(self, repo_address, user, repo):
+        self._repo_address = repo_address
         self._user = user
         self._repo = repo
+
+    def get_repo_address(self):
+        return self._repo_address
 
     def get_user(self):
         return self._user
 
     def get_repo(self):
         return self._repo
+
+    @classmethod
+    def new_from_repo_address(cls, repo_address):
+        user = GithubRepository.get_user_from_repo_address(repo_address)
+        repo = GithubRepository.get_repo_from_repo_address(repo_address, user)
+        if user is None or repo is None:
+            return None
+        return GithubRepository(repo_address, user, repo)
+
+    @classmethod
+    def get_user_from_repo_address(cls, repo_address):
+        user_match = re.search('(?<=github\.com.)(.*)(?=/)', repo_address) # match anything after github.com until /
+        if user_match is not None:
+                return user_match.group(0)
+        else:
+            return None
+
+    @classmethod
+    def get_repo_from_repo_address(cls, repo_address, user):
+        repo_match = re.search("(?<=%s/)(.*)(?=\.git)" % user, repo_address) # match <something>.git after user/
+        if repo_match is not None:
+            return repo_match.group(0)
+        else:
+            return None
 
 def git_repo_addresses():
     return SPLUNKGIT_GIT_SETTINGS['repo_addresses']
@@ -53,37 +81,15 @@ def splunk_password():
 def github_user_repos():
     space_separated_repo_addresses = git_repo_addresses()
     repo_addresses = space_separated_repo_addresses.split(' ')
-    return github_user_repos_from_repo_addresses(repo_addresses)
+    return github_repos_from_repo_addresses(repo_addresses)
 
-def github_user_repos_from_repo_addresses(repo_addresses):
-    github_user_repos = []
+def github_repos_from_repo_addresses(repo_addresses):
+    github_repos = []
     for repo_address in repo_addresses:
-        github_user_repo = github_user_repo_from_repo_address(repo_address)
-        if github_user_repo is not None:
-            github_user_repos.append(github_user_repo)
-    return github_user_repos
-
-def github_user_repo_from_repo_address(repo_address):
-    user = get_user_from_repo_address(repo_address)
-    if user is not None:
-        repo = get_repo_from_repo_address(repo_address, user)
-        if repo is not None:
-            return GithubUserRepo(user, repo)
-    return None
-
-def get_user_from_repo_address(repo_address):
-    user_match = re.search('(?<=github\.com.)(.*)(?=/)', repo_address) # match anything after github.com until /
-    if user_match is not None:
-        return user_match.group(0)
-    else:
-        return None
-
-def get_repo_from_repo_address(repo_address, user):
-    repo_match = re.search("(?<=%s/)(.*)(?=\.git)" % user, repo_address) # match <something>.git after user/
-    if repo_match is not None:
-        return repo_match.group(0)
-    else:
-        return None
+        github_repo = GithubRepository.new_from_repo_address(repo_address)
+        if github_repo is not None:
+            github_repos.append(github_repo)
+    return github_repos
 
 if __name__ == '__main__':
     print git_repo_addresses()
