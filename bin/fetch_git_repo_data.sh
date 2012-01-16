8e96d4956393f2410d6c8d640bbedfdc5d09da4c
Petter Eriksson <periksson@splunk.com>
2012-01-16 13:11:14 -0800

    Separated fetching git data and generating multi repo view.
    The generation of the multi repository view can, and
    is, now set to only be run on start up. Which allows
    for editing the view inside of Splunk and the view
    not disappearing sometimes.
        
    Issue #6 on Github

--- bin/fetch_git_repo_data.sh
+++ bin/fetch_git_repo_data.sh
@@ -22,24 +22,14 @@ SCRIPT_HOME=$(dirname $0)
 APP_HOME=`splunk cmd ./$SCRIPT_HOME/app_home.sh`
 APP_NAME=`echo $APP_HOME | sed 's/.*\///'`
 
-# Splunk authentication
-username_password_script="splunk cmd python $SCRIPT_HOME/print_splunk_user_and_password.py"
-SPLUNK_USERNAME=`$username_password_script | grep -oP '^[^:]+'`
-SPLUNK_PASSWORD=`$username_password_script | grep -oP '(?<=:)(.*)'`
-
 #Initializing
 GIT_REPO=
 GIT_REPO_FOLDER=
 GIT_REPOS_HOME=
 chosen_repository=
 
-#XML writing for a view that views all repositories
-xml_dir=$APP_HOME/local/data/ui/views
-xml_file=$xml_dir/multi_repositories.xml
-
 main ()
 {
-setup_xml
 for repository in `splunk cmd python $SCRIPT_HOME/splunkgit_settings.py`
 do
   echo "fetching git repo data for repository: $repository" 1>&2
@@ -58,30 +48,7 @@ do
       fetch_git_repository
     fi
   fi
-  write_xml $repository
 done
-end_xml
-}
-
-setup_xml () {
-  # Create xml file
-  mkdir -p $xml_dir
-  echo "<?xml version='1.0' encoding='utf-8'?>" > $xml_file
-  echo "<dashboard>" >> $xml_file
-  echo "  <label>Repositories</label>" >> $xml_file
-}
-
-# Write multi_repositories_row.txt and replace ---REPOSITORY--- with $1, which should be a repository
-write_xml () {
-  repository=$1
-  repository_simple_name=`echo $repository | sed 's/.*\///' | sed 's,\.git,,'`
-  cat $APP_HOME/bin/multi_repositories_row.txt | sed "s,---REPOSITORY---,$repository," | sed "s,---REPOSITORY_SIMPLE_NAME---,$repository_simple_name,"  >> $xml_file
-}
-
-end_xml () {
-  echo "</dashboard>" >> $xml_file
-# Reload views for $APP_NAME (splunkgit)
-  curl -s -u $SPLUNK_USERNAME:$SPLUNK_PASSWORD -k https://localhost:8089/servicesNS/nobody/$APP_NAME/data/ui/views/_reload > /dev/null
 }
 
 #Not safe to run this method parallel from the same directory, since the $numstat_file is touched, written to and deleted.
---
+++ bin/generate_multi_repo_view.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Copyright 2011 Splunk, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Script for generating a view that views all repositories that are configured in splunkgit.conf
+# Author: Petter Eriksson
+
+#Global variables
+SCRIPT_HOME=$(dirname $0)
+APP_HOME=`splunk cmd ./$SCRIPT_HOME/app_home.sh`
+
+# Splunk authentication
+username_password_script="splunk cmd python $SCRIPT_HOME/print_splunk_user_and_password.py"
+SPLUNK_USERNAME=`$username_password_script | grep -oP '^[^:]+'`
+SPLUNK_PASSWORD=`$username_password_script | grep -oP '(?<=:)(.*)'`
+
+#XML writing for a view that views all repositories
+xml_dir=$APP_HOME/local/data/ui/views
+xml_file=$xml_dir/multi_repositories.xml
+
+main ()
+{
+  setup_xml
+  for repository in `splunk cmd python $SCRIPT_HOME/splunkgit_settings.py`
+  do
+    write_xml $repository
+  done
+  end_xml
+}
+
+setup_xml () {
+  # Create xml file
+  mkdir -p $xml_dir
+  echo "<?xml version='1.0' encoding='utf-8'?>" > $xml_file
+  echo "<dashboard>" >> $xml_file
+  echo "  <label>Repositories</label>" >> $xml_file
+}
+
+# Write multi_repositories_row.txt and replace ---REPOSITORY--- with $1, which should be a repository
+write_xml () {
+  repository=$1
+  repository_simple_name=`echo $repository | sed 's/.*\///' | sed 's,\.git,,'`
+  cat $APP_HOME/bin/multi_repositories_row.txt | sed "s,---REPOSITORY---,$repository," | sed "s,---REPOSITORY_SIMPLE_NAME---,$repository_simple_name,"  >> $xml_file
+}
+
+
+end_xml () {
+  echo "</dashboard>" >> $xml_file
+  # Reload views for $APP_NAME (splunkgit)
+  curl -s -u $SPLUNK_USERNAME:$SPLUNK_PASSWORD -k https://localhost:8089/servicesNS/nobody/$APP_NAME/data/ui/views/_reload > /dev/null
+}
+
+# Run script
+main
